60f0d48cc155cb15b129cfb65f8f5d00a4048216
Sushma <sushma@sushmas-air.lan>
2018-08-04 11:24:32 -0700

    .

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var createError = require('http-errors');
+//var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,7 +14,7 @@ app.listen(3010);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
