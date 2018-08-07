f8776a6b695b609a343eb6234038229947d7897b
Sushma <sushma@sushmas-air.lan>
2018-08-06 23:20:56 -0700

    new app to get weather

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var fs = require("fs");
+var request = require('request');
+var fetchJson = require('node-fetch-json');
 
 var app = express();
 app.listen(3010);
@@ -28,12 +30,39 @@ app.get('/suma', function(req,res){
   res.sendFile(p);
 });
 
-app.get('/loadSuma', function(req, res){
-  // Get content from file
-  var contents = fs.readFileSync("test.json");
-  // Define to JSON type
-  var jsonContent = JSON.parse(contents);
-  res.json(jsonContent);
+
+var schema = {"coord":{"lon":139,"lat":35},
+"sys":{"country":"JP","sunrise":1369769524,"sunset":1369821049},
+"weather":[{"id":804,"main":"clouds","description":"overcast clouds","icon":"04n"}],
+"main":{"temp":289.5,"humidity":89,"pressure":1013,"temp_min":287.04,"temp_max":292.04},
+"wind":{"speed":7.31,"deg":187.002},
+"rain":{"3h":0},
+"clouds":{"all":92},
+"dt":1369824698,
+"id":1851632,
+"name":"Shuzenji",
+"cod":200};
+
+app.get('/weather', function(req,res){
+  p = path.join(__dirname,'public/pages/weather.html');
+  res.sendFile(p);
+})
+
+
+app.post('/updateWeather', function(req,res){
+  var location = req.body.location;
+  var key = '5793fc98014d229079a27929038a79a1'
+  var url = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + key;
+  console.log(url);
+  request(url, function (err, response, body) {
+    if(err){
+      console.log('error:', error);
+    } else {
+      body = JSON.parse(body);
+      var temp_f = (body.main.temp*9/5) - 459.67;
+      res.render("weather", {temp: temp_f, loc: location});
+    }
+  });
 });
 
 app.post('/updateWhatever', function(req, res){
@@ -66,3 +95,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
--- public/pages/core.js
+++ public/pages/core.js
@@ -4,11 +4,32 @@ otfApp.controller('otfController', ['$scope', '$http', function($scope, $http) {
     $scope.user = "Sushma";
     $scope.class_options = ["Endurance", "Strength", "Power", "ESP", "Special"];
     $scope.data_obj ={};
-    $http.get('/loadSuma').
+    $scope.getWeather = function(){
+       /*$http.get("https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b6907d289e10d714a6e88b30761fae22").
+        then(function(success){
+            $scope.data_obj = success.data;
+            $scope.addEntry = function(){
+                $scope.data_obj.push($scope.class);
+            }      
+        });*/
+    }
+    /*$http.get('/loadSuma').
     then(function(success){
         $scope.data_obj = success.data;
         $scope.addEntry = function(){
             $scope.data_obj.push($scope.class);
         }      
-    });       
+    }); */      
 }]);
+
+var weatherApp = angular.module('weatherApp',[]);
+
+weatherApp.controller('weatherController', ['$scope', '$http', function($scope, $http){
+    $scope.getWeather = function(){
+        $http.get("/updateWeather").
+        then(function(success){
+            console.log(success.data);
+        })
+    }
+}]);
+
